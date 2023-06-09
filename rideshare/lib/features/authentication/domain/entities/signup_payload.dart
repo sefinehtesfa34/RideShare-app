ce2ca4403e0e90f34d7fce4e8fc3d39054bf0779
Yared <83911480+Yared-betsega@users.noreply.github.com>
2023-06-09 15:33:17 +0300

    Fix(Mobile-RideShare): Add new fields on signup page (#9)
    * (Mobile-RideShare): Add new fields on signup page
    
    * (Mobile-Dartknights): Add id fields

--- rideshare/lib/features/authentication/domain/entities/signup_payload.dart
+++ rideshare/lib/features/authentication/domain/entities/signup_payload.dart
@@ -1,10 +1,21 @@
+import 'dart:io';
+
 import 'package:equatable/equatable.dart';
 
 class SignupPayload extends Equatable {
   final String fullName;
   final String phoneNumber;
+  final int age;
+  final String sex;
+  final File id;
 
-  const SignupPayload({required this.fullName, required this.phoneNumber});
+  const SignupPayload({
+    required this.fullName,
+    required this.phoneNumber,
+    required this.sex,
+    required this.age,
+    required this.id,
+  });
   @override
-  List<Object> get props => [fullName, phoneNumber];
+  List<Object> get props => <Object>[fullName, phoneNumber, sex, age, id];
 }
---
+++ rideshare/test/features/authentication/domain/usecases/signup_test.dart
@@ -0,0 +1,69 @@
+import 'dart:io';
+
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:rideshare/core/errors/failures.dart';
+import 'package:rideshare/features/authentication/domain/entities/login_payload.dart';
+import 'package:rideshare/features/authentication/domain/entities/signup_payload.dart';
+import 'package:rideshare/features/authentication/domain/repositories/authentication_repository.dart';
+import 'package:rideshare/features/authentication/domain/usecases/signup.dart';
+
+import 'login_test.mocks.dart';
+import 'signup_test.mocks.dart';
+
+@GenerateMocks(<Type>[File])
+void main() {
+  late Signup signup;
+  late MockAuthenticationRepository mockRepository;
+
+  final MockFile mockFile = MockFile();
+  final SignupPayload user = SignupPayload(
+    fullName: "testName",
+    phoneNumber: "+251923423433",
+    sex: "M",
+    age: 22,
+    id: mockFile,
+  );
+  setUp(() {
+    mockRepository = MockAuthenticationRepository();
+    signup = Signup(mockRepository);
+  });
+  test("Should create a new user", () async {
+    when(mockRepository.signup(user)).thenAnswer((_) async => Right(user));
+
+    final Either<Failure, SignupPayload> result = await signup(user);
+    expect(result, Right(user));
+    verify(mockRepository.signup(user));
+    verifyNoMoreInteractions(mockRepository);
+  });
+
+  test("Should return server failure when user cannot signup", () async {
+    when(mockRepository.signup(user)).thenAnswer(
+        (_) async => const Left(ServerFailure("Internal Server Failure")));
+
+    final Either<Failure, SignupPayload> result = await signup(user);
+    expect(result, const Left(ServerFailure("Internal Server Failure")));
+    verify(mockRepository.signup(user));
+    verifyNoMoreInteractions(mockRepository);
+  });
+  test("Should return InputFailure when invalid credentials are provided",
+      () async {
+    SignupPayload user = SignupPayload(
+      fullName: "invalid",
+      phoneNumber: "invalid",
+      sex: "F",
+      age: 17,
+      id: mockFile,
+    );
+
+    when(mockRepository.signup(user))
+        .thenAnswer((_) async => const Left(InputFailure("Invalid Data")));
+
+    final Either<Failure, SignupPayload> result = await signup(user);
+    expect(result, const Left(InputFailure("Invalid Data")));
+    verify(mockRepository.signup(user));
+    verifyNoMoreInteractions(mockRepository);
+  });
+}
