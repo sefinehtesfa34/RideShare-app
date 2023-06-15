6e51bc0190c37a97e2bd743917b1c3a6ecdc5002
Yared Tsegaye <yaredtsegaye120@gmail.com>
2023-06-15 23:02:45 +0300

    (Mobile:RideShare):add wiidges

--- rideshare/lib/features/authentication/presentation/screens/sign_up_page.dart
+++ rideshare/lib/features/authentication/presentation/screens/sign_up_page.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
+import 'package:image_picker/image_picker.dart';
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
@@ -8,192 +12,342 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  File imageFile = File("");
+  String pickImageText = "Insert Image";
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Scaffold(
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 50, left: 20),
-              child: Text(
-                'Sign Up',
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: Theme.of(context).primaryColor,
-                ),
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(
+            height: 4.h,
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 3.h, bottom: 0.5.h),
+            child: Text(
+              'Sign Up',
+              style: TextStyle(
+                fontSize: 25.sp,
+                fontWeight: FontWeight.w600,
+                fontFamily: "Poppins",
+                color: Theme.of(context).primaryColor,
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(left: 20, bottom: 20),
-              child: Text(
-                'Create account',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: const Color(0xFF6E80B0),
-                ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(bottom: 1.h),
+            child: Text(
+              'Create account',
+              style: TextStyle(
+                fontSize: 18.sp,
+                fontFamily: "Poppins",
+                color: const Color(0xFF6E80B0),
               ),
             ),
-            Stack(
-              alignment: Alignment.bottomRight,
-              children: [
+          ),
+          Stack(
+            alignment: Alignment.center,
+            children: <Widget>[
+              GestureDetector(
+                onTap: () async {
+                  final ImagePicker picker = ImagePicker();
+                  final XFile? pickedImage =
+                      await picker.pickImage(source: ImageSource.gallery);
+                  if (pickedImage != null) {
+                    setState(() {
+                      imageFile = File(pickedImage.path);
+                      pickImageText = "";
+                    });
+                  }
+                },
+                // ignore: unnecessary_null_comparison
+                child: imageFile.path != ""
+                    ? Image.file(
+                        imageFile,
+                        fit: BoxFit.cover,
+                        width: 14.w,
+                        height: 12.h,
+                      )
+                    : Container(
+                        width: 20.w,
+                        height: 12.h,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                            image: AssetImage('assets/images/profile_icon.png'),
+                          ),
+                        ),
+                        child: const Align(
+                          alignment: Alignment.bottomRight,
+                          child: Icon(
+                            Icons.edit_outlined,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+              ),
+            ],
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Text(
+              pickImageText,
+              style: TextStyle(
+                fontSize: 15.sp,
+                fontFamily: "Poppins",
+                color: Colors.black,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 7.w),
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 2.h),
+                const AcceptText(
+                    label: 'Full name', hintText: "Insert full name"),
+                SizedBox(height: 2.h),
+                const AcceptText(label: 'Age', hintText: "Insert age"),
+                SizedBox(height: 2.h),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    const Text(
+                      "Sex",
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontFamily: "Poppins",
+                          fontWeight: FontWeight.w600),
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          width: 25.w,
+                          height: 6.h,
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).primaryColor,
+                            borderRadius: BorderRadius.circular(4.w),
+                            border: Border.all(
+                              color: Theme.of(context).primaryColor,
+                              width: 2,
+                            ),
+                          ),
+                          child: Center(
+                            child: Text(
+                              'Male',
+                              style: TextStyle(
+                                fontSize: 16.sp,
+                                fontFamily: "Poppins",
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 3.h),
+                        Container(
+                          width: 25.w,
+                          height: 6.h,
+                          decoration: BoxDecoration(
+                            color: const Color.fromRGBO(239, 239, 250, 1),
+                            borderRadius: BorderRadius.circular(4.w),
+                          ),
+                          child: Center(
+                            child: Text(
+                              'Female',
+                              style: TextStyle(
+                                fontSize: 16.sp,
+                                color: const Color.fromRGBO(128, 144, 186, 1),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(height: 2.h),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "Phone Number",
+                      style: TextStyle(
+                          fontSize: 16.sp,
+                          fontFamily: "Poppins",
+                          fontWeight: FontWeight.w600),
+                    ),
+                    Row(
+                      children: <Widget>[
+                        const Expanded(
+                          child: AcceptText(label: '', hintText: "+251"),
+                        ),
+                        SizedBox(width: 2.h),
+                        const Expanded(
+                          flex: 5,
+                          child: AcceptText(label: '', hintText: ""),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(height: 5.h),
                 Container(
-                  width: 150,
-                  height: 150,
+                  height: 8.h,
+                  width: double.infinity,
                   decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                      image: AssetImage('assets/images/profile_icon.png'),
-                      fit: BoxFit.cover,
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(4.w),
+                    border: Border.all(
+                      color: Theme.of(context).primaryColor,
+                      width: 2,
                     ),
                   ),
-                  child: Icon(
-                    Icons.edit,
-                    color: Colors.white,
+                  child: InkWell(
+                    child: Center(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Expanded(
+                            flex: 2,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: <Widget>[
+                                Text(
+                                  "Submit",
+                                  style: TextStyle(
+                                      fontSize: 20.sp, color: Colors.white),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: <Widget>[
+                                Padding(
+                                  padding: EdgeInsets.only(right: 1.w),
+                                  child: const Image(
+                                      image: AssetImage(
+                                          "assets/images/right_arrow.png")),
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 10, right: 10),
-                  child: Text(
-                    'Insert image',
-                    style: TextStyle(
-                      fontSize: 16,
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AcceptText extends StatelessWidget {
+  const AcceptText({
+    super.key,
+    required this.label,
+    required this.hintText,
+  });
+
+  final String label;
+  final String hintText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          label,
+          style: const TextStyle(
+              fontSize: 16, fontFamily: "Poppins", fontWeight: FontWeight.w600),
+        ),
+        Container(
+          height: 6.5.h,
+          decoration: BoxDecoration(
+            color: const Color.fromRGBO(239, 239, 250, 1),
+            borderRadius: BorderRadius.circular(5.w),
+          ),
+          child: TextField(
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.only(left: 4.w),
+              hintText: hintText,
+              hintStyle: TextStyle(
+                  fontFamily: "Poppins",
+                  fontSize: 15.5.sp,
+                  color: const Color.fromRGBO(128, 144, 186, 1)),
+              border: InputBorder.none,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class PickImage extends StatelessWidget {
+  const PickImage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          "ID",
+          style: TextStyle(
+              fontSize: 16.sp,
+              fontFamily: "Poppins",
+              fontWeight: FontWeight.w600),
+        ),
+        Container(
+          height: 6.h,
+          decoration: BoxDecoration(
+            color: const Color.fromRGBO(239, 239, 250, 1),
+            borderRadius: BorderRadius.circular(5.w),
+          ),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 5.w),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'Pick ID Image',
+                  style: TextStyle(
+                    fontSize: 16.sp,
+                    fontFamily: "Poppins",
+                    color: const Color.fromRGBO(128, 144, 186, 1),
+                  ),
+                ),
+                SizedBox(height: 2.h),
+                GestureDetector(
+                  onTap: () async {
+                    final ImagePicker picker = ImagePicker();
+                    final XFile? pickedImage =
+                        await picker.pickImage(source: ImageSource.gallery);
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Icon(
+                      Icons.add,
+                      size: 4.w,
                       color: Colors.white,
                     ),
                   ),
                 ),
               ],
-            ),
-            SizedBox(height: 20),
-            _buildTextField('Full name'),
-            SizedBox(height: 10),
-            _buildTextField('Sex'),
-            SizedBox(height: 10),
-            Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    height: 50,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      border: Border.all(
-                        color: Theme.of(context).primaryColor,
-                        width: 2,
-                      ),
-                    ),
-                    child: Center(
-                      child: Text(
-                        'Male',
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Theme.of(context).primaryColor,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(width: 10),
-                Expanded(
-                  child: Container(
-                    height: 50,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      border: Border.all(
-                        color: Colors.grey[400]!,
-                        width: 2,
-                      ),
-                    ),
-                    child: Center(
-                      child: Text(
-                        'Female',
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.grey[400],
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 10),
-            Row(
-              children: [
-                Expanded(
-                  child: _buildTextField('Country code'),
-                ),
-                SizedBox(width: 10),
-                Expanded(
-                  flex: 2,
-                  child: _buildTextField('Phone number'),
-                ),
-              ],
-            ),
-            SizedBox(height: 10),
-            _buildTextField('ID'),
-            SizedBox(height: 10),
-            Row(
-              children: [
-                Container(
-                  width: 50,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
-                    shape: BoxShape.circle,
-                  ),
-                  child: Icon(
-                    Icons.add,
-                    color: Colors.white,
-                  ),
-                ),
-                SizedBox(width: 10),
-                Text(
-                  'Insert image',
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Theme.of(context).primaryColor,
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildTextField(String label) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(left: 20, bottom: 5),
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 16,
-              color: Colors.grey[600],
-            ),
-          ),
-        ),
-        Container(
-          height: 50,
-          margin: const EdgeInsets.symmetric(horizontal: 20),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            border: Border.all(
-              color: Colors.grey[400]!,
-              width: 2,
-            ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: TextField(
-              decoration: InputDecoration(
-                border: InputBorder.none,
-              ),
             ),
           ),
         ),
