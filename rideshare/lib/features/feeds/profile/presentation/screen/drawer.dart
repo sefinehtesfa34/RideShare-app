c3209bb19c2ed1eec8da8ba11c0629e3ebd913e7
Sefineh Tesfa <sefineh.tesfa@a2sv.org>
2023-06-20 16:23:57 +0300

    [Mobile] logout page

--- rideshare/lib/features/feeds/profile/presentation/screen/drawer.dart
+++ rideshare/lib/features/feeds/profile/presentation/screen/drawer.dart
@@ -4,15 +4,85 @@ import 'package:responsive_sizer/responsive_sizer.dart';
 import '../../../../../constants/drawer_items.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({
-    super.key,
-  });
+  const MyHomePage({Key? key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Future<void> _showLogoutConfirmationDialog() async {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return Center(
+          child: AlertDialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(
+                16.sp,
+              ), // Set the desired border radius here
+            ),
+            contentPadding: EdgeInsets.symmetric(
+              vertical: 20.sp, // Set the desired vertical padding here
+              horizontal: 20.sp, // Set the desired horizontal padding here
+            ),
+            content: const Text('Are you sure you want to logout?'),
+            actions: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(bottom: 20.sp),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: const Color(0xFF6D61F2)),
+                        borderRadius: BorderRadius.circular(8.sp),
+                      ),
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop(); // Close the dialog
+                        },
+                        child: const Text(
+                          'Cancel',
+                          style: TextStyle(
+                            color: Color(0xFF6D61F2),
+                            fontFamily: 'Poppins',
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 3.w,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: const Color(0xFFEA4335)),
+                        borderRadius: BorderRadius.circular(8.sp),
+                      ),
+                      child: TextButton(
+                        onPressed: () {
+                          // Perform logout operation here
+                          Navigator.of(context).pop(); // Close the dialog
+                        },
+                        child: const Text(
+                          'Logout',
+                          style: TextStyle(
+                            color: Color(0xFFEA4335),
+                            fontFamily: 'Poppins',
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -80,8 +150,34 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               );
             }
-
             final DrawerMenuItem menuItem = menuItems[index - 1];
+            if (menuItem.title == 'Log out') {
+              return Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 6.h, // Set the desired height here
+                    child: ListTile(
+                      leading: menuItem.icon,
+                      title: Text(
+                        menuItem.title,
+                        style: TextStyle(
+                          color: const Color(0xFF414141),
+                          fontSize: 16.sp,
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      onTap: _showLogoutConfirmationDialog,
+                    ),
+                  ),
+                  const Divider(
+                    color: Colors.grey,
+                    thickness: 1,
+                  )
+                ],
+              );
+            }
+
             return Column(
               children: <Widget>[
                 SizedBox(
@@ -91,10 +187,11 @@ class _MyHomePageState extends State<MyHomePage> {
                     title: Text(
                       menuItem.title,
                       style: TextStyle(
-                          color: const Color(0xFF414141),
-                          fontSize: 16.sp,
-                          fontFamily: 'Poppins',
-                          fontWeight: FontWeight.w500),
+                        color: const Color(0xFF414141),
+                        fontSize: 16.sp,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w500,
+                      ),
                     ),
                     onTap: menuItem.onTap,
                   ),
