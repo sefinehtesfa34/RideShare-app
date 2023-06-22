2b1b91a38e67f5071e15da20ba8268ba9a521595
Sefineh Tesfa <sefineh.tesfa@a2sv.org>
2023-06-22 13:16:10 +0300

    [Mobile] conflict resolved

--- rideshare/lib/main.dart
+++ rideshare/lib/main.dart
@@ -1,12 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:rideshare/features/authentication/presentation/bloc/signup/sign_up_bloc.dart';
-import 'package:rideshare/features/feeds/location/presentation/bloc/back_to_location/bloc/back_to_location_bloc.dart';
-import 'package:rideshare/features/feeds/location/presentation/bloc/location_bloc.dart';
-import 'package:rideshare/features/feeds/location/presentation/screen/picking_location.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
-
-import 'features/feeds/profile/presentation/screen/drawer.dart';
 import 'features/feeds/profile/presentation/screen/passengers_history_page.dart';
 
 void main() {
