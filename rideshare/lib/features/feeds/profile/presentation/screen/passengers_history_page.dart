ded674fed8360d6b79d487970a88d08cbb52e018
Sefineh Tesfa <sefineh.tesfa@a2sv.org>
2023-06-22 13:21:28 +0300

    [Mobile] conflict resolved

--- rideshare/lib/features/feeds/profile/presentation/screen/passengers_history_page.dart
+++ rideshare/lib/features/feeds/profile/presentation/screen/passengers_history_page.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
-import 'package:rideshare/features/feeds/profile/presentation/widgets/rides_history.dart';
 
 import '../../../../../constants/json.dart';
+import '../widgets/histsory_list_tile.dart';
 import '../widgets/special_appbar.dart';
 
 class HistoryPage extends StatefulWidget {
@@ -32,62 +31,7 @@ class _HistoryPageState extends State<HistoryPage> {
                 itemBuilder: (BuildContext context, int index) {
                   final Map<String, String> historyItem = historyList[index];
 
-                  return Padding(
-                    padding: EdgeInsets.only(left: 18.0.sp, right: 16.sp),
-                    child: Column(
-                      children: <Widget>[
-                        TextButton(
-                          onPressed: () {},
-                          style: ButtonStyle(
-                            padding: MaterialStateProperty.all<EdgeInsets>(
-                              EdgeInsets.zero,
-                            ),
-                          ),
-                          child: Row(
-                            children: <Widget>[
-                              Column(
-                                children: <Widget>[
-                                  SvgPicture.asset(
-                                    'assets/images/current_mocation_marker.svg',
-                                  ),
-                                  SizedBox(
-                                    height: 0.5.h,
-                                  ),
-                                  SvgPicture.asset(
-                                    'assets/images/Subtract.svg',
-                                  ),
-                                ],
-                              ),
-                              SizedBox(width: 2.w),
-                              Expanded(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: <Widget>[
-                                    RidesHistory(
-                                        text: historyItem['SourceIcon'] ?? ''),
-                                    SizedBox(height: 1.h),
-                                    RidesHistory(
-                                        text: historyItem['DestinationIcon'] ??''),
-                                  ],
-                                ),
-                              ),
-                              SizedBox(width: 2.w),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.end,
-                                children: <Widget>[
-                                  RidesHistory(
-                                      text: historyItem['DateTime'] ?? ''),
-                                  RidesHistory(
-                                      text: historyItem['Price'] ?? ''),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                        const Divider(),
-                      ],
-                    ),
-                  );
+                  return HistoryListTile(historyItem: historyItem);
                 },
               ),
             ),
--- rideshare/lib/main.dart
+++ rideshare/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 import 'package:rideshare/features/feeds/profile/presentation/bloc/update_profile_bloc.dart';
-import 'features/feeds/profile/presentation/screen/passenger_profile.dart';
+import 'features/feeds/profile/presentation/screen/drawer.dart';
 import 'injection_container.dart';
 
 void main() {
@@ -23,9 +23,9 @@ class MyApp extends StatelessWidget {
           create: (BuildContext context) => GetIt.instance(),
           child: const MaterialApp(
             title: 'Image Picker Demo',
-            // home: MyHomePage(),
+            home: MyHomePage(),
             // home: HistoryPage(),
-            home: ProfilePage(),
+            // home: ProfilePage(),
           ),
         );
       }),
