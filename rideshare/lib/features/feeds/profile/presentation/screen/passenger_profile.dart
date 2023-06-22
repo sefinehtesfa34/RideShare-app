b31bb4564581c127b813245063ac9ca50b6b86c9
Sefineh Tesfa <sefineh.tesfa@a2sv.org>
2023-06-22 20:56:07 +0300

    [Mobile] clean codes

--- rideshare/lib/features/feeds/profile/presentation/screen/passenger_profile.dart
+++ rideshare/lib/features/feeds/profile/presentation/screen/passenger_profile.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
-
 import '../../../location/presentation/widgets/select_button.dart';
 import '../bloc/update_profile_bloc.dart';
 import '../bloc/update_profile_state.dart';
