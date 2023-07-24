fb366d6c62901f3ca13c68716ee5940a3e84234c
Hiwot Derese <Hiwotderese16@gmail.com>
2023-07-24 15:52:01 +0300

    fix search driver functionality on driver side

---
+++ rideshare_driver/lib/core/injections/pick_passenger_injection.dart
@@ -0,0 +1,29 @@
+import '../../features/pick_passengers/data/datasource/ride_offer_datasource.dart';
+import '../../features/pick_passengers/data/repositories/ride_offer_repository.dart';
+import '../../features/pick_passengers/domain/repository/ride_offer.dart';
+import '../../features/pick_passengers/domain/usecase/add_passenger_usecase.dart';
+import '../../features/pick_passengers/domain/usecase/drop_passenger_usecase.dart';
+import '../../features/pick_passengers/domain/usecase/get_ride_offers_usecase.dart';
+import '../../features/pick_passengers/presentation/bloc/fetch_passengers/fetch_passengers_bloc.dart';
+import '../../features/pick_passengers/presentation/bloc/pick_passengers/pick_passengers_bloc.dart';
+import '../../features/pick_passengers/presentation/bloc/sorting_selector/sorting_selector_bloc.dart';
+import 'injection_container.dart';
+
+PickPassengersInit() {
+  sl.registerFactory(() => FetchPassengersBloc(sl()));
+  sl.registerFactory(() => SortingSelectorBloc());
+  sl.registerFactory(() => PickPassengersBloc(sl()));
+
+  sl.registerLazySingleton(() => GetRideOffersUseCase(sl()));
+  sl.registerLazySingleton(() => AddPassengerUseCase(sl()));
+
+  sl.registerLazySingleton(() => DropPassengerUseCase(sl()));
+  
+
+  sl.registerLazySingleton<RideOfferRepository>(
+      () => RideOfferRepositoryImpl(sl()));
+
+  sl.registerLazySingleton<RideOfferApi>(
+    () => RideOfferApiImpl(baseUrl: "https://rideshare-app.onrender.com"),
+  );
+}
